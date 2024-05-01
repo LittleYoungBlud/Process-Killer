cc75b91f5d53ef725b4a55c58bd4f2fb120d65ea
LittleYoungBlud <162307055+LittleYoungBlud@users.noreply.github.com>
2024-05-01 02:03:45 -0700

    Add files via upload

---
+++ update.bat
@@ -0,0 +1,12 @@
+@echo off
+
+REM Download the latest version of the batch file
+powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/LittleYoungBlud/Process-Killer/main/vipers_task_killer_1.bat' -OutFile 'newbatchfile.bat'"
+
+REM Replace the current batch file with the new one
+move /y newbatchfile.bat yourbatchfile.bat
+
+REM Clean up
+del newbatchfile.bat
+
+echo Update complete.
