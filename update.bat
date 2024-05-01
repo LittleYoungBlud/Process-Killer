37ec4f79dd0a14d49b08d4f54016239a20680a51
LittleYoungBlud <162307055+LittleYoungBlud@users.noreply.github.com>
2024-05-01 02:07:23 -0700

    Update update.bat

--- update.bat
+++ update.bat
@@ -4,7 +4,7 @@ REM Download the latest version of the batch file
 powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/LittleYoungBlud/Process-Killer/main/vipers_task_killer_1.bat' -OutFile 'vipers_task_killer_1.bat'"
 
 REM Replace the current batch file with the new one
-move /y newbatchfile.bat yourbatchfile.bat
+move /y vipers_task_killer_1.bat vipers_task_killer_1.bat
 
 REM Clean up
 del vipers_task_killer_1.bat
