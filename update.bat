40198ba5ba63e2e0f2705b303928c9aaba2bad0e
LittleYoungBlud <162307055+LittleYoungBlud@users.noreply.github.com>
2024-05-01 02:27:52 -0700

    Update update.bat

--- update.bat
+++ update.bat
@@ -5,16 +5,26 @@ echo Deleting old version...
 del "vipers_task_killer_1.bat" > nul
 
 REM Download the latest version of the batch file
-echo Installing new updated batch file...
-powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/LittleYoungBlud/Process-Killer/main/vipers_task_killer_1.bat' -OutFile 'vipers_task_killer_1.bat'"
+echo Checking for updates...
+powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/LittleYoungBlud/Process-Killer/main/vipers_task_killer_1.bat' -OutFile 'new_version.bat' -UseBasicParsing"
 
-REM Flash blue 3 times
-for /l %%i in (1,1,3) do (
-    color 1F
-    timeout /t 1 /nobreak >nul
-    color 0F
-    timeout /t 1 /nobreak >nul
+REM Check if there are changes
+fc "vipers_task_killer_1.bat" "new_version.bat" > nul
+if errorlevel 1 (
+    echo Installing new updated batch file...
+    move /y "new_version.bat" "vipers_task_killer_1.bat" > nul
+    
+    REM Flash blue 3 times
+    for /l %%i in (1,1,3) do (
+        color 1F
+        timeout /t 1 /nobreak >nul
+        color 0F
+        timeout /t 1 /nobreak >nul
+    )
+
+    echo Update complete.
+    pause
+) else (
+    echo You have the latest version. Check back later.
+    pause
 )
-
-echo Update complete.
-pause
